1a73eff0ac70db45bafe05d4cbaa256c2556b9b0
Richard Simões <rsimoes@cpan.org>
2013-02-19 02:27:33 -0600

    frist

---
+++ lib/Alien/LibYAML.pm
@@ -0,0 +1,12 @@
+package Alien::LibYAML;
+
+use v5.10;
+use strict;
+use warnings FATAL => "all";
+use utf8;
+use parent "Alien::Base";
+
+# VERSION
+# ABSTRACT: Build and install libyaml, a C-based YAML parser and emitter
+
+1;
