2e09cf175a84f9ddacc425030c46b228db94518f
Richard Simões <rsimoes@cpan.org>
2014-03-21 12:59:01 -0500

    minor cleanup

--- lib/Alien/LibYAML.pm
+++ lib/Alien/LibYAML.pm
@@ -2,9 +2,9 @@ package Alien::LibYAML;
 
 use v5.10;
 use strict;
-use warnings FATAL => "all";
+use warnings FATAL => 'all';
 use utf8;
-use parent "Alien::Base";
+use parent qw(Alien::Base);
 
 # VERSION
 # ABSTRACT: Build and install libyaml, a C-based YAML parser and emitter
